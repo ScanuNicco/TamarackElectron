d5468b6563c903ab5d2233dce457fb1399ebaf36
scanunicco <Scanuproductions@gmail.com>
2022-02-14 16:20:25 -0800

    Use grep to speed things up

--- nukeHashtags.sh
+++ nukeHashtags.sh
@@ -3,11 +3,14 @@ cd "$1"
 for artist in *; do 
     for album in "$artist"/*; do
     	if [ -d "$album" ]; then 
-		echo "$album"
+		echo "Searching in $album"
 		for song in "$album"/*; do
 			if [ -f "$song" ]; then
-				renamed=$(echo "$song" | sed "s/#//g")
-        			mv "$1/$song" "$1/$renamed"
+				if grep -q "#" <<<"$song"; then
+					echo "Renaming $song"
+					renamed=$(echo "$song" | sed "s/#//g") #Remove hashtags
+        				mv "$1/$song" "$1/$renamed"
+				fi
 			fi
 		done
 	fi
